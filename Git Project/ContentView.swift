53984a692d2aab9584bea471856e1ca1ca5e29b4
Shane Simpson <shanes.mailbox@gmail.com>
2021-01-09 10:08:19 -0800

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Shane Simpson on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isPresented = false
+    
+    var body: some View {
+        
+        NavigationView{
+            Button("Show Modal") {
+                self.isPresented = true
+                
+            }.sheet(isPresented: $isPresented){
+                Text("This is the modal screen")
+                
+            }
+        } // End Navigation View
+        
+    } // End View
+} // End Content View
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Shane Simpson on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
